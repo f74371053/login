7882f9da66d79317a2dcb10101833ab2ae2861f3
f74371053 <a0921916155@gmail.com>
2016-12-22 15:15:27 +0800

    qq

--- member_update.php
+++ member_update.php
@@ -1,3 +1,20 @@
 <td class="tdrline">
 <form action="updatelink.php" method="POST" name="formJoin" id="formJoin" onsubmit="return ckeckForm();">
-  
+  <p class="title">修改資料
+  </p>
+  <div class="dataDiv">
+    <hr size="1"  />
+    <p class="heading">帳號資料
+    </p>
+    <p><strong>使用帳號</strong>
+      :<?php echo $row_RecMember["m_username"];?>
+    </p>
+    <p><strong>使用密碼</strong>:
+      <input name="m_password" type="password" class="normalinput" id="m_password">
+      <br>
+    </p>
+    <p><strong>確認密碼</strong>:
+      <input name="m_passwordrecheck" type="password" class="normalinput" id="m_passwordrecheck">
+      <br>
+    </p>
+    
